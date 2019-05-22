0d2b430ec1fe519e3124454bec43def9f957ffbb
Ahab <Ahab1109@163.com>
2019-05-22 11:00:38 +0800

    Delete picture.py

--- picture.py
+++
@@ -1,8 +0,0 @@
-from MyQR import myqr
-myqr.run(
-
-words='https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzAwNjg1NTA1Mw==&scene=126#wechat_redirect',
- picture='1.gif',
- colorized=True,
- save_name='artistic.gif',
-)
